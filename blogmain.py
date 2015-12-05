ae99852baf44d88ac17f50e577d1ee95bbf77cd4
Jason <jianchuli0624@gmail.com>
2015-12-05 17:08:35 -0500

    add login without cookie

--- blogmain.py
+++ blogmain.py
@@ -3,4 +3,5 @@ import webapp2
 import blogpage
 import signup
 
-app = webapp2.WSGIApplication([('/blog', blogpage.BlogPage),('/',blogpage.BlogPage), ('/signup',signup.SignUp),('/welcome',signup.Welcome)], debug=True)
+app = webapp2.WSGIApplication([('/blog', blogpage.BlogPage),('/',blogpage.BlogPage), ('/signup',signup.SignUp),('/welcome/([a-zA-Z0-9_-]{3,20}$)',signup.Welcome),('/login',signup.Login)], debug=True)
+
--- signup.py
+++ signup.py
@@ -14,8 +14,8 @@ def username_check(username):
 
 def username_exist_check(username):
 	query = db.GqlQuery("SELECT * FROM User WHERE username = :username",username=username)
-	first = query.get()
-	if first:
+	user = query.get()
+	if user:
 		return False
 	return True
 
@@ -34,14 +34,50 @@ def email_check(email):
 		return True
 	return False
 
+def check_login(username, password):
+	query = db.GqlQuery("SELECT * FROM User WHERE username = :username",username=username)
+	user = query.get()
+	if user.password == password:
+		return True
+	return False
+
+
 def get_total_user():
 	return models.User.all().count()
 
-
 class Welcome(handler.Handler):
-	def get(self):
-		self.render("welcome.html")
+	def get(self, username):
+		self.render("welcome.html", username = username)
 
+class Login(handler.Handler):
+	def render_login(self, username = "", username_error = "", password_error = ""):
+		self.render("login.html", username = username, username_error = username_error, password_error = password_error)
+
+	def get(self):
+		self.render_login()
+
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+		username_error = ""
+		password_error = ""
+		error_flag = True
+		if (not username_check(username)):
+			username_error = "That's not a valid username."
+			error_flag = False
+		elif (username_exist_check(username)):
+			username_error = "The user doesn't exist."
+			error_flag = False			
+		if (not password_check(password)):
+			password_error = "That's not a valid password."
+			error_flag = False
+		elif ((not username_exist_check(username)) and (not check_login(username, password))):
+			password_error = "The passowrd doesn't match the username."
+			error_flag = False
+		if (not error_flag): 
+			self.render_login(username, username_error, password_error)
+			return 
+		self.redirect("/welcome/%s" % str(username))		
 
 class SignUp(handler.Handler):
 	"""handle signup page"""
@@ -83,6 +119,9 @@ class SignUp(handler.Handler):
 		if (not error_flag):
 			self.render_sign_up(username, email, username_error, password_error, password_verify_error, email_error)
 			return
-		new_user = models.User(key_mame = get_total_user() + 1, user_id = get_total_user() + 1, username = username, password = password, email = email)
+		key_mame = get_total_user() + 1
+		new_user = models.User(key_mame = str(username), user_id = key_mame, username = username, password = password, email = email)
 		new_user.put()
-		self.redirect("/welcome")
+		print new_user.key()
+		# self.response.headers.add_header('Set-Cookie', 'user_id=%s' % str(key_mame))
+		self.redirect("/welcome/%s" % str(username))
