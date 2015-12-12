8c673ef37b2a793e3fdee4484c116ece5b1a20b4
Jason <jianchuli0624@gmail.com>
2015-12-12 18:08:53 -0500

    add NYT not well

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,8 @@ var wiki = 'https://en.wikipedia.org/w/api.php?format=json&action=query&titles=%
 var objToday = new Date(),
             weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
             dayOfWeek = weekday[objToday.getDay()],
-            domEnder = new Array( 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th' ),
+            domEnder = new Array( 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th' ),
+            // domEnder = new Array( 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th' ),
             dayOfMonth = today + (objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder[objToday.getDate()] : objToday.getDate() + domEnder[parseFloat(("" + objToday.getDate()).substr(("" + objToday.getDate()).length - 1))],
             months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
             curMonth = months[objToday.getMonth()],
@@ -23,8 +24,11 @@ function loadData(){
 		var lat = data.lat;
 		var lon = data.lon;
 		$weather.append('<div>' + data.isp + ', ' + data.city + ', ' + data.regionName + ', ' + data.country + '</div>');
+		$('#province').val(data.regionName);
+		$('#city').val(data.city);
 		getWeather(lat, lon);
 		getMap(data.isp, data.city);
+		
 	}).error(function(e){
 		alert("Error!");
 	});
@@ -48,10 +52,11 @@ function getWeather(lat, lon){
 
 function getMap(isp, city){
 	var $weather = $('#weather');
+	var $map = $('#map');
 	isp = isp.replace(/ /g, "+");
-	city = city.replace(/ /g, "+");
+	city = city.replace(/ /g, "+"); 
 	GMAPS_URL = "http://maps.google.com/maps/api/staticmap?size=380x380&center=%isp%,%city%&zoom=15&sensor=false".replace("%isp%", isp).replace("%city%", city);
-	$weather.append('<img id = "map" src = %url%>'.replace("%url%", GMAPS_URL));
+	$map.append('<img id = "map" src = %url%>'.replace("%url%", GMAPS_URL));
 
 }
 
@@ -60,7 +65,7 @@ function loadWikiEvents(){
 	var $births = $('#births');
 	var $holidays = $('#holidays');
 
-	currentDate_event = (curMonth + "_" + dayOfMonth).replace("st", "");
+	currentDate_event = (curMonth + "_" + dayOfMonth).replace("th", "");
 	wiki_events = wiki.replace("%content%", currentDate_event);
 
     var wikitimeout = setTimeout(function(){
@@ -84,11 +89,12 @@ function loadWikiEvents(){
     		
     		events = content.substring(index_events,index_births).replace(/\[/g,"").replace(/\]/g,"").split('\n');
     		births = content.substring(index_births,index_deaths).replace(/\[/g,"").replace(/\]/g,"").split('\n');
-    		if (index_references){
+    		
+    		if (index_references >0){
     			holidays = content.substring(index_holidays,index_references).replace(/\[/g,"").replace(/\]/g,"").split('\n');
     		} else{
     			holidays = content.substring(index_holidays,index_external).replace(/\[/g,"").replace(/\]/g,"").split('\n');
-    		}	
+    		}
 
     		for (var i = 0; i < 10; i++){
     			$events.append(events[i] + "<br>");
@@ -107,29 +113,50 @@ function loadWikiEvents(){
     		}
     		$holidays.append("<div class = 'more'> <a href='https://en.wikipedia.org/wiki/%content%'> more </a> </div>".replace("%content%",currentDate_event+"#Holidays_and_observances"));
     		$holidays.prepend("<br><br>")
-
-    		// var events = content.replace(/==Events==([\s\S]*?)[[361]]/, "$1");
-
-
-    		//var articleList = response[1];
-    		// for (var i =0; i<articleList.length;i++){
-    		// 	arricleStr = articleList[i];
-
-    		// 	console.log(articleList.length);
-    		// 	var url = 'http://en.wikipedia.org/wiki/' + arricleStr;
-    		// $events.append(events);
     		clearTimeout(wikitimeout);
     	}
 
     });
-
-	// console.log(wiki_events);
 }
 
 
+function loadNYT(){
+	var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+
+    var currentprovince = $('#province').val();
+    var currentCity = $('#city').val();    
+	NYTURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+currentCity +'+' + currentprovince +'&page=2&sort=oldest&api-key=dff1a8407c15258bfc80ef76a5936b3d:12:73559609'
+    $.getJSON(NYTURL, function(data){
+    	$nytHeaderElem.text(currentCity + " news in The New York Times");
+    	articles = data.response.docs;
+    	for (var i =0 ;i<articles.length;i++){
+    		var article = articles[i];
+    		$nytElem.append('<li class = "article">' + '<a href="' + article.web_url + '">' + article.headline.main+'</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+    	};
+    }).error(function(e){
+    	   $nytHeaderElem.text("What?!");
+    });
+	return false;
+};
+
+// function getLocation(){
+// 	ipapi_url = "http://ip-api.com/json";
+// 	var city;
+// 	var region;
+
+// 	$.getJSON(ipapi_url, function(data){
+// 			console.log(city);
+// 		return data.city;
+// 		region = data.regionName;
+// 	}).error(function(e){
+// 		alert("Error!");
+// 	});
+// 	return city;
+// };
+
 if (window.location.protocol != "http:")
     window.location.href = "http:" + window.location.href.substring(window.location.protocol.length)
-
 loadData();
 loadWikiEvents();
-
+$('#form-container').submit(loadNYT);
